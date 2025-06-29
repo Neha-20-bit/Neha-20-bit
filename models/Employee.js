575289275058300e5fbe2c299f7884440621cccb
Neha-20-bit <nnegi5988@gmail.com>
2025-06-29 15:34:13 +0530

    Add files via upload

---
+++ models/Employee.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const employeeSchema = new mongoose.Schema({
+  name: String,
+  position: String,
+  department: String,
+  salary: Number
+});
+module.exports = mongoose.model('Employee', employeeSchema);
